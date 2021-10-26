69eec84f8ddb644ca29a6f00dec847fd34eb87bc
Davey <daveyzaal@hotmail.nl>
2021-10-26 23:24:10 +0200

    made a better fetch

--- week1/dataset.js
+++ week1/dataset.js
@@ -1,4 +1,4 @@
-[
+const data = [
   {
     "Wat is je favoriete soort huisdier?": "kat",
     "Wat is je oogkleur?": "Bruin",
--- week1/index.js
+++ week1/index.js
@@ -1,15 +1,27 @@
-console.log("mogge");
+const url =
+  "https://raw.githubusercontent.com/DeefDeMeef/functional-programming/main/week1/dataset.json?token=AHKPTBV4EZMO2RNHYRYCURDBQEGZM";
 
-fetch("./data.json")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    // Work with JSON data here
-    console.log(data);
-  })
-  .catch((err) => {
-    // Do something for an error here
+const collectData = () => {
+  fetch(url)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Er is iets misgegaan...");
+      }
+    })
+    .then((res) => {
+      getAnswersMethod(res);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const getAnswersMethod = (obj) => {
+  obj.map((item) => {
+    console.log(item["Wat is je favoriete soort huisdier?"]);
   });
+};
 
-renderData();
+collectData();
