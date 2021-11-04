fe1dbfae17f3f964de5fd35c97684884270dee5a
Davey <daveyzaal@hotmail.nl>
2021-11-04 14:45:40 +0100

    redirect uri fix

--- week2/src/config.js
+++ week2/src/config.js
@@ -2,6 +2,6 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = "8f2bfa15b6f441ecb4ad98b8bcf9e7c0";
-// export const redirectUri = "6183252f4cf7991108f1680a--wizardly-pasteur-388676.netlify.app/redirect";
-export const redirectUri = "http://localhost:3000/redirect";
+export const redirectUri = "https://functional-programming-6oyx6.ondigitalocean.app/redirect";
+// export const redirectUri = "http://localhost:3000/redirect";
 export const scopes = ["user-top-read", "user-read-currently-playing", "user-read-playback-state"];
